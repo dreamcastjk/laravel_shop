a19d318d0f55619c38c3ac9e268fba731d342922
Stanislav Z <slipdoll@gmail.com>
2020-06-25 17:02:34 +0300

    js and css

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -10,14 +10,14 @@
     <title>{{ config('app.name', 'Laravel') }}</title>
 
     <!-- Scripts -->
-    <script src="{{ asset('build/public/js/app.js') }}" defer></script>
+    <script src="{{ mix('public/js/app.js', 'build') }}" defer></script>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.googleapis.com/css?family=Nunito" rel="stylesheet">
 
     <!-- Styles -->
-    <link href="{{ asset('build/public/css/app.css') }}" rel="stylesheet">
+    <link href="{{ mix('public/css/app.css', 'build') }}" rel="stylesheet">
 </head>
 <body>
     <div id="app">
