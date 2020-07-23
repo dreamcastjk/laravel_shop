d06b99b3d1eb58c07e775b0f6e956bfc05f7353e
Zhiharev <slipdoll@gmail.com>
2020-07-23 17:13:36 +0300

    breadcrumbs

--- routes/breadcrumbs.php
+++ routes/breadcrumbs.php
@@ -1,17 +1,32 @@
 <?php
 
-use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator;
+use DaveJamesMiller\Breadcrumbs\BreadcrumbsGenerator as Crumbs;
 
-Breadcrumbs::register('home', function (BreadcrumbsGenerator $crumbs) {
+Breadcrumbs::register('home', function (Crumbs $crumbs) {
     $crumbs->push('Home', route('home'));
 });
 
-Breadcrumbs::register('login', function (BreadcrumbsGenerator $crumbs) {
+Breadcrumbs::register('login', function (Crumbs $crumbs) {
     $crumbs->parent('home');
     $crumbs->push('Login', route('login'));
 });
 
-Breadcrumbs::register('password_reset', function (BreadcrumbsGenerator $crumbs) {
+Breadcrumbs::register('register', function (Crumbs $crumbs) {
+    $crumbs->parent('home');
+    $crumbs->push('Register', route('register'));
+});
+
+BreadCrumbs::register('password.request', function (Crumbs $crumbs) {
     $crumbs->parent('login');
-    $crumbs->push('Reset Password', route('password.request'));
+    $crumbs->push('Reset Password', route('register'));
+});
+
+BreadCrumbs::register(('password.reset'), function (Crumbs $crumbs) {
+    $crumbs->parent('password.request');
+    $crumbs->push('Change', route('password.reset'));
+});
+
+BreadCrumbs::register(('password.reset'), function (Crumbs $crumbs) {
+    $crumbs->parent('home');
+    $crumbs->push('Cabinet', route('cabinet'));
 });
